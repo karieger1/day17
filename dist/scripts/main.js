1b1936fa180144ccd327877ccc8d62c2ae2c7430
Kristen Rieger <karieger1@gmail.com>
2015-06-01 23:06:45 -0500

    final commit for tonight

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,26 +1,47 @@
 $(window).load(function() {
-		$("main").hide();
-		$("#loader").delay(4000).fadeOut("medium");
-		$("main").delay(4000).fadeIn("medium");
-	});
+			$("main").hide();
+			$("#loader").delay(4000).fadeOut("slow");
+			$("main").delay(4000).fadeIn("slow");
+		});
+
+$(document).on("ready",(function() {
+
+		var routerConfig = Backbone.Router.extend({
+			routes: {
+				"":              "goHome",
+				"home":          "goHome",
+				"play":          "goPlay",   
+				"leaderboard":   "goLeaderboard",   
+				"settings":      "goSettings"
+			},
+		
 
 
-$(document).ready(function() {
-	var routerConfig = {
-		routes: {
-			"":              "goHome",
-			"play":          "goPlay",   
-			"leaderboard":   "goLeaderboard",   
-			"settings":      "goSettings"
+		goHome: function() {
+			$('#home').show();
+			$('.stuff').hide();
 		},
-	}
-});
+
+		goPlay: function() {
+			$('.stuff').hide();
+			$('#home').hide(); 
+			$('#play-game').show(); 
+		}, 
+
+		goLeaderboard: function() {
+			$('.stuff').hide();
+			$('#home').hide();
+			$('#leaderboard').show();
+		}, 
+
+		goSettings: function() {
+			$('.stuff').hide();
+			$('#home').hide();
+			$('#settings').show();
+		}
+
+	}); 
 
 
-
-// $(document).ready(function() {
-// 	 $("#loader").delay(4000).animate({
-// 	        opacity:0,
-// 	        width: 0,
-// 	        height:0
-//     }, 500);
+	
+}));
